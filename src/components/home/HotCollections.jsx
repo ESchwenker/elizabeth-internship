b31cd2faa8acc18f00986ae957aa6a071b38e3bf
ESchwenker <Eschwenker24@outlook.com>
2026-02-06 16:50:26 -0500

    HotCollections done.

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import {useKeenSlider} from "keen-slider/react";
+import SkeletonCard from "../UI/SkeletonCard.jsx"
+
+
 
 const HotCollections = () => {
+  const [collections, setCollections] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  const [sliderRef, slider] = useKeenSlider({
+    loop: true,
+    slides: {
+      perView: 4,
+      spacing: 15,
+    },
+    mode: "snap",
+    breakpoints: {
+    "(max-width: 1200px)": {
+      slides: {
+        perView: 3,
+        spacing: 15,
+      },
+    },
+
+    "(max-width: 768px)": {
+      slides: {
+        perView: 2,
+        spacing: 10,
+      },
+    },
+
+    "(max-width: 480px)": {
+      slides: {
+        perView: 1,
+        spacing: 5,
+      },
+    },
+  },
+  });
+
+  useEffect(() => {
+  if (!loading && slider.current) {
+    slider.current.update();
+  }
+  }, [loading, slider]);
+
+  useEffect(() => {
+    async function getCollections() {
+      try {
+        const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections");
+
+      setCollections(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
+    getCollections();
+  }, []);
+
+
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +76,42 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+          <div className="carousel-wrapper">
+              <button className="carousel-arrow left"
+              onClick={() => slider.current?.prev()}>&#10094;</button>
+            <div ref={sliderRef} className="keen-slider">
+              {loading?  new Array(4).fill(0).map((_, index) => (
+                <div className="keen-slider__slide" key={index}>
+                  <SkeletonCard />
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
+              ))
+              : collections.map(collection => (
+                <div className="keen-slider__slide" key={collection.id}>
+                    <div className="nft_coll">
+                      <div className="nft_wrap">
+                        <Link to="/item-details">
+                          <img src={collection.nftImage} className="lazy img-fluid" alt="" />
+                        </Link>
+                      </div>
+                      <div className="nft_coll_pp">
+                        <Link to="/author">
+                          <img className="lazy pp-coll" src={collection.authorImage} alt="" />
+                        </Link>
+                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="nft_coll_info">
+                        <Link to="/explore">
+                          <h4>{collection.title}</h4>
+                        </Link>
+                        <span>ERC-{collection.code}</span>
+                      </div>
+                    </div>
                 </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
+              ))}
             </div>
-          ))}
+            <button className="carousel-arrow right"
+              onClick={() => slider.current?.next()}>&#10095;</button>
+          </div>
         </div>
       </div>
     </section>
